77bb01797f368700e259d17a867806765d52e4ec
ABCFS\DanielC <Daniel.Crouch@abcfinancial.com>
2019-04-30 09:31:12 -0500

    Added compare arrows

--- src/javascript/compare.js
+++ src/javascript/compare.js
@@ -1,4 +1,5 @@
 var relevantList = [];
+var relevantListFull = [];
 
 function compareRankings( type )
 {
@@ -12,12 +13,15 @@ function setRelevantList( type )
     {
     case "Disney":
         relevantList = disneyList.map( movie => movie.title );
+        relevantListFull = disneyList;
         break;
     case "Marvel":
         relevantList = marvelList.map( movie => movie.title );
+        relevantListFull = marvelList;
         break;
     case "StarWars":
         relevantList = starWarsList.map( movie => movie.title );
+        relevantListFull = starWarsList;
     }
 }
 
@@ -29,8 +33,28 @@ function openCompareModal()
     blurBackground();
 
     var listDiv = $('#modalList');
-    listDiv.append( "<img width='1em' src='images/up.png'   alt='up'>" );
-    listDiv.append( "<img width='1em' src='images/down.png' alt='down'>" );
+    listDiv.empty();
+    listDiv.css( "text-align", "center" );
+    for ( var i = 0; i < relevantList.length; i++ )
+    {
+        listDiv.append( "<div id='listItem" + i + "' style='display: flex; flex-direction: row; justify-content: center; margin-bottom: .5em'></div>" );
+        var listItem = $('#listItem' + i);
+
+        listItem.append( "<div id='arrows" + i + "' style='display: flex; flex-direction: column; justify-content: center; margin-right: .2em'></div>" );
+        var arrowDiv = $('#arrows' + i);
+        arrowDiv.append( "<img class='clickable' style='width: 1em' src='images/up.png'   alt='up'    onclick='moveItem( " + i + ", true  )'>" );
+        arrowDiv.append( "<img class='clickable' style='width: 1em' src='images/down.png' alt='down'  onclick='moveItem( " + i + ", false )'>" );
+
+        listItem.append( "<div id='item" + i + "'></div>" );
+        var itemDiv = $('#item' + i);
+        itemDiv.append( "<img style='width: 6em' src='" + relevantListFull[i].image + "' title='" + relevantList[i] + "' alt='" + relevantList[i] + "'>" );
+    }
+}
+
+function moveItem( index, isUp )
+{
+    var item = $('#listItem' + index);
+    isUp ? item.prev().insertAfter( item ) : item.next().insertBefore( item );
 }
 
 function compareText()
@@ -43,17 +67,25 @@ function compareText()
 
 function scoreRankings()
 {
+    var answers = [];
+    $('div[id*="listItem"]').each(function(){
+        var title = relevantList[this.id.substring(8)];
+        answers.push( title );
+    });
+
     closeModal( $('#compareModal') );
-
-    //todo - get their rankings
-
-    displayResults();
+    scoreAnswers( answers, false );
 }
 
 function scoreTextRankings( answer )
 {
-    var sum = 0;
     var answers = answer.split('\n').map( movie => movie.trim() ).filter( Boolean );
+    scoreAnswers( answers, true );
+}
+
+function scoreAnswers( answers, isText )
+{
+    var sum = 0;
     var count = relevantList.length;
 
     for ( var i = 0; i < count; i++ )
@@ -74,13 +106,14 @@ function scoreTextRankings( answer )
     score = ( score + 1 ) * 50;
     score = ( score < 100 ) ? score.toPrecision( 4 ) : 100;
 
-    displayResults( score, answers );
+    displayResults( score, answers, isText );
 }
 
-function displayResults( score, rankings )
+function displayResults( score, rankings, isText )
 {
-    //todo - display side-by-side images with my rankings
+    var resultsDisplay = rankings.join("<br/>"); //todo - isText ? rankings.join("<br/>") : *Display as side-byside images*
     showConfirm( "Comparison Results", "If 0% is the opposite and 100% a total match, our rankings match:" +
                                        "<div class='center' style='font-size: 1.5em'>" + score + "%</div>\n" +
-                                       rankings.join("<br/>") );
+                                       "<br/>" +
+                                       resultsDisplay );
 }
